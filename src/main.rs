acfc32a360a5da71907f67fdacd6c07f81d0c164
부익 김 <laplace1009@gmail.com>
2024-06-13 23:46:06 +0900

    / 라우터에 Hello, World 설정

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async {"Hello, World!"}));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Listening on 3000 port");
+    axum::serve(listener, app).await.unwrap();
 }

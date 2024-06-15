ae26a32dc2572a90ea41159823c8374234ae3025
부익 김 <laplace1009@gmail.com>
2024-06-15 16:41:25 +0900

    dash 로 동영상 파일 segment 분할 전송

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,16 @@ use tokio::{
 async fn main() {
     let app = Router::new()
         .route("/", get(|| async {"Hello, Axum"}))
-        .route("/video", get(video_sample));
+        .route("/video", get(video_sample))
+        .route("/dash/manifest.mpd", get(get_dash_manifest))
+        .route("/dash/:segment", get(get_dash_segment));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Listening on 3000 port");
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn video_sample() -> Result<Response, StatusCode> {
-    let file_path = "video/sample_480p.mp4";
+    let file_path = "static/video.html";
     let mut file = match File::open(&file_path).await {
         Ok(file) => file,
         Err(_) => return Err(StatusCode::NOT_FOUND)
@@ -30,6 +32,36 @@ async fn video_sample() -> Result<Response, StatusCode> {
     if let Err(_) = file.read_to_end(&mut buffer).await {
         return Err(StatusCode::INTERNAL_SERVER_ERROR)
     }
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", HeaderValue::from_static("text/html"));
+    Ok((headers, buffer).into_response())
+}
+
+async fn get_dash_manifest() -> Result<Response, StatusCode> {
+    let file_path = "video/manifest.mpd";
+    let mut file = match File::open(&file_path).await {
+        Ok(file) => file,
+        Err(_) => return Err(StatusCode::NOT_FOUND),
+    };
+    let mut buffer = Vec::new();
+    if let Err(_) = file.read_to_end(&mut buffer).await {
+        return Err(StatusCode::INTERNAL_SERVER_ERROR);
+    };
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", HeaderValue::from_static("application/dash+xml"));
+    Ok((headers, buffer).into_response())
+}
+
+async fn get_dash_segment(uri: axum::extract::Path<String>) -> Result<Response, StatusCode> {
+    let file_path = format!("video/{}", uri.0);
+    let mut file = match File::open(&file_path).await {
+        Ok(file) => file,
+        Err(_) => return Err(StatusCode::NOT_FOUND),
+    };
+    let mut buffer = Vec::new();
+    if let Err(_) = file.read_to_end(&mut buffer).await {
+        return Err(StatusCode::INTERNAL_SERVER_ERROR);
+    };
     let mut headers = HeaderMap::new();
     headers.insert("Content-Type", HeaderValue::from_static("video/mp4"));
     Ok((headers, buffer).into_response())
